1c51795a6fe10392fb7ec6e6a2ef20b902380e82
Vladimir <mr.vladimir1337@gmail.com>
2022-09-30 18:32:37 +0300

    added prefix,fixed LED trigger,changed LED3 and LED6 to LED3 and LED7

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,13 @@ int main()
 {
   LedControl ledSet;
   ledSet.initialize();
-  ledSet.set(00100100);
-  
+  /*
+  1 is off
+  0 is on
+  0b [led8] [led7] [led6] [led5]
+     [led4] [led3] [led2] [led1]
+  */
+  ledSet.set(0b10111011);
+
   return 0;
 }
